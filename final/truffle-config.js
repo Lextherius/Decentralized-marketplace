64955ef6f9a0047b8f6f4dfe160e8713f761e66a
Alex Yap <registrar@lextherius.com>
2018-09-07 12:09:24 +0800

    ^ update truffle config

--- final/truffle-config.js
+++ final/truffle-config.js
@@ -1,10 +1,24 @@
 module.exports = {
   migrations_directory: "./migrations",
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 2000
+    }
+  },
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      host: "127.0.0.1",
+      port: 7545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 3,
+      gas: 1828127,
+      from: "0xa114236135f3018bf8d8f9b36f55120f9b3ca695" // replace with your Testnet account address
     }
   }
+
 };
